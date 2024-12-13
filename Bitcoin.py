c7b09009daf186aaba5d1d464e1bcf1a1d159af0
Edgar Rios <edgarrios@Edgars-MacBook-Pro.local>
2024-12-13 15:56:31 -0500

    adding Bitcoin ticker

--- Bitcoin.py
+++ Bitcoin.py
@@ -3,21 +3,3 @@ multiplier = 1
 timespan = "day"
 start_date = "2024-01-09"
 end_date = "2024-12-10"
-
-url = f"https://api.polygon.io/v2/aggs/ticker/{ticker}/range/{multiplier}/{timespan}/{start_date}/{end_date}?apiKey={API_Key}"
-
-#print(url)
-
-response = requests.get(url)
-data = response.json()
-
-if "results" in data:
-    df = pd.DataFrame(data["results"])
-
-    df['t'] = pd.to_datetime(df['t'], unit='ms')
-
-    df.rename(columns={'t': 'Date', 'o': 'Open', 'h': 'High', 'l': 'Low', 'c': 'Close', 'v': 'Volume'}, inplace=True)
-
-    print(df.head())
-else:
-    print("Error fetching data:", data)
